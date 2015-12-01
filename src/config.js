3562832f88c151f2df32a4815e0725f9842be3be
Moses Grant <mosesgrant3345@gmail.com>
2015-11-30 19:13:31 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,18 @@
+"Configuration":function(config){
+  config.pop("Configuration");
+  if(config.length > 1){
+    config.find("../build");
+  }
+  if(config.find("../build"); === true){
+    config.open("../build");
+  }
+  return config.exec("Configuration");
+}
+
+for(var e=0;e.length<"Configuration"; e++){
+  e.push("../build.bat");
+  if(e === 0){ //simple?
+  e.find(0);
+  e+=1;
+  return e.back("next config file");
+}
