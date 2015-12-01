c31df2db945f0139944720d83124bc0f0acc2889
Moses Grant <mosesgrant3345@gmail.com>
2015-12-01 06:11:23 -0500

    tool for utilizing initialization of files

--- tools/init_tool.js
+++ tools/init_tool.js
@@ -1,5 +1,12 @@
-function init_tool(file){
-  file.push("bin/comet")
+var createInit = {
+  init.create:"init.create();"
 }
+function init_tool(file){
+  file.push("bin/comet");
+  if(1 < 2){
+    file.go(file); 
+  }
+}
+var makeInit = new createInit;
 
-
+init_tool(makeInit);
