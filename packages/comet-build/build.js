87dc4dc2483946f8721c9dd79b94a807fe299828
Moses Grant <mosesgrant3345@gmail.com>
2015-11-30 19:22:45 -0500

    startup and push build for repo

---
+++ packages/comet-build/build.js
@@ -0,0 +1,11 @@
+var repository = "";
+function createBuild(repo){
+  var build = require("../comet-build");
+  var comet = require("bin/comet")
+  if(build === true){
+    repo.open(build);
+    repo.compile(build);
+  }
+  repo + comet.debug(repository);
+}
+createBuild(repository);
