bc51de1cf22d5e3fcb08f9450b9cfc0e615a3189
Moses Grant <mosesgrant3345@gmail.com>
2015-11-30 20:54:55 -0500

    v1.0-stable modules

--- packages/comet-build/modules_stable.js
+++ packages/comet-build/modules_stable.js
@@ -1,3 +1,20 @@
 var cmd = {
-  compile:
+  cmp:"cmd.compile()"
 }
+var input = "";
+cmd.cmpl = function(command){
+  if(input === cmd.cmp){
+    cmd.cmp.fontcolor("purple");
+    input.compile(cmd.cmp);
+  }
+  cmd.cmp.parse(input);
+  command = input + cmd.cmp;
+  for(var i=0;i<command.length; i++){
+    command.push(input);
+  }
+  while(cmd.cmp.parse(input)){
+    input.parse(command);
+    input.confirm(cmd.cmp);
+  }
+}
+cmd.cmpl(cmd.cmp);
