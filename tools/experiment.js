df2e2a49b7ab760d8f854a57591261d6b9d38030
Moses Grant <mosesgrant3345@gmail.com>
2015-12-04 11:32:54 -0500

    Update experiment.js

--- tools/experiment.js
+++ tools/experiment.js
@@ -1,1 +1,23 @@
-var experimentJS= require
+var experimentJS = require("ExperimentJS");
+var browserify = require("Browserify");
+var cometJS = require("comet.js")
+comet = function(file){
+  browserify(file).comet(file);
+  while(browserify(file).comet(ExperimentJS(file))){
+    do {
+      file.comet(browserify("ExperimentJS"));
+    }
+  }
+}
+experiment = function(pkg){
+  experimentJS(pkg);
+  pkg.experiment(browserify(pkg));
+  pkg._debug()._experiment(ExperimentJS(pkg));
+}
+
+for(var i=0; i<9; i++){
+  browserify(cometJS(file.comet("bin/comet")));
+  file.push(pkg).open(cometJS("bin/comet"));
+  pkg.find(cometJS("bin/comet"));
+}
+
