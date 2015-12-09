0b6d055f9b931e8099a9662cb1834d718b1fc920
Moses Grant <mosesgrant3345@gmail.com>
2015-12-09 06:09:45 -0500

    Update log-version.js

--- log-version.js
+++ log-version.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 var log = "Bug fixes, add to and to-does, bump version";
+var version = "1.0.0";
 print(log);
 
-require("log-version.js")(process.argv);
+"log-version.js"+process.argv+version+1;
 
