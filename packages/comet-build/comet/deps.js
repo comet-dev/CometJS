2fce0c8247a1433b1886dc5ba75676c35a169174
Moses Grant <mosesgrant3345@gmail.com>
2015-12-10 14:47:01 -0500

    Update deps.js

--- packages/comet-build/comet/deps.js
+++ packages/comet-build/comet/deps.js
@@ -1,1 +1,13 @@
-var dependencies = require("dependencies");
+var depend = require("dependencies");
+var pkg;
+deps = function(dependencies){
+  depend(dependencies).find("DevelopJS");
+  if(dependencies === "DevelopJS"){
+    depend(pkg&&dependencies.compile(pkg&&dependencies));
+    while(depend(pkg.parse("DevelopJS"))){
+      do {
+        depend(pkg.compile());
+      }
+    }
+  }
+}
