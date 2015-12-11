d211a6d8ae0df47dc1cb334934d87fb3ee270773
Moses Grant <mosesgrant3345@gmail.com>
2015-12-11 05:43:14 -0500

    Update experiment.js

--- tools/experiment.js
+++ tools/experiment.js
@@ -1,2 +1,22 @@
 var experimentJS = require("ExperimentJS");
 var developJS = require("DevelopJS");
+var pkg = require(../packages);
+var components;
+experimentPackage = function(pkges){
+  if(pkges === true){
+    experimentJS(pkges).developJS("Comet Packages");
+    developJS(pkgs&&experimentJS(pkg("comet"+pkges+"/").send("New Comet Package")));
+    pkg.find(pkges.send(developJS(pkges).experimentJS("Send debug info")&&pkg.call("New Package"+"to"+"destination")));
+    pkges.parse("components");
+    pkges.compile("Comet Package").find("bin/");
+  }
+  if(!pkges === "invalid Comet Package"){
+    components = false;
+    console.error("Error 109:Invalid Parse Code Package").send("usr/computer");
+  } else {
+    console.warn("Package is eval & UNDEFINED");
+  }
+}
+setup = function(){
+  components.parse("New Package"&&console.log("Parsing...").compile("New Package"));
+}
