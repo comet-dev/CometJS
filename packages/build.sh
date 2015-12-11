165d9162f0bf56f17015bcb843041bc47f0a0e67
Moses Grant <mosesgrant3345@gmail.com>
2015-12-11 07:21:53 -0500

    Update build.sh

--- packages/build.sh
+++ packages/build.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+buildPackage = "./comet-build/build"
+
+if[$buildPackage == "False"];
+  then 
+  mkdir $buildPackage 
+  find $buildPackage
+  
+addAPackage(){
+  $buildPackage == "True";
+  compress $buildPackage
+  unzip $buildPackage
+}  
