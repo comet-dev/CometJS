80b7d9b1e1be543896cf80525aa9671a2bde1012
Moses Grant <mosesgrant3345@gmail.com>
2015-12-12 18:34:03 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,9 @@
-
+var packageContains = create({
+  init_tool:"./tools/init_tool",
+  experiment:"./tools/experiment.js"
+});
+customizePackage = function(){
+  var install = require("install");
+  install(packageContains.experiment).push(console.log("New Dev"));
+  console.warn("Customizing package may alter the way the package works.");
+}
