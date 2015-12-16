8b4d9d799119e252179fa445728fe11160d6decf
Moses Grant <mosesgrant3345@gmail.com>
2015-12-16 08:39:21 -0500

    Update notifications.js

--- packages/comet-update-notification/notifications.js
+++ packages/comet-update-notification/notifications.js
@@ -1,3 +1,5 @@
-notifications = function(){
-
+var version = require("update.js")
+notifications = function(notify){
+  make.prototype.box = notify.alert("Comet has been update to " + version("update.js"));
 }
+notifications(version("update.js"));
