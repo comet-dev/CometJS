3409f1c52b3cfabb44824c6955b93ee9c319ddae
Moses Grant <mosesgrant3345@gmail.com>
2015-12-18 08:19:42 -0500

    Update make_package.js

--- src/make_package.js
+++ src/make_package.js
@@ -1,14 +1,13 @@
+import CometPackages from Comet
 var userInput = "";
-var makePackagefunction = "make.package()";
 var pkgs = create({
   pkgsbuild: "build a package",
   pkgsinstall: "install a package"
 });
 make.package = function(pkg){
-  pkg.button("Make a package");
-  pkg.dropDown("Build A Package", "Install A Package");
-  pkg.click("Build A Package")
-  if(userInput === makePackagefunction){
-    makePackagefunction.fontcolor("red");
+  pkg.push("NewPackage");
+  if(pkg === "New"){
+    userInput.comet("Initialization");
+  }
   }
 }
