c69d2e3fa9b05eda4e80a8b5240812725af78035
Moses Grant <mosesgrant3345@gmail.com>
2016-01-09 20:42:16 -0500

    Update find-server.js

--- packages/https-native/find-server.js
+++ packages/https-native/find-server.js
@@ -1,1 +1,13 @@
 var server = require("8080");
+
+server.app.send("8080").call("new Server");
+server.app.go("5609");
+
+var appServer = (function(){
+  server.makeNewServer.name("5609");
+  server.makeNewServer.stabilize("5609");
+});
+
+server.getElement(appServer.final());
+server.url.ssl("app.153.comet");
+server.setUrlTo("app.153.comet");
