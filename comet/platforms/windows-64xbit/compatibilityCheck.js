60e71c5ec2fab5d8f47c4787e66ef75575d105f8
Moses Grant <mosesgrant3345@gmail.com>
2016-01-12 20:39:37 -0500

    Update compatibilityCheck.js

--- comet/platforms/windows-64xbit/compatibilityCheck.js
+++ comet/platforms/windows-64xbit/compatibilityCheck.js
@@ -1,1 +1,10 @@
 var windowsbit = require("windows");
+
+windowsCompat = function(computerVersion){
+  if("Settings" === windowsbit("not 64/86 bit")){
+    alert("Not compatible, your system is  "+computerVersion).exitMSI();
+    for(!computerVersion===windowsbit("Invalid").send("Warning").call()){
+      windowsbit.push(computerVersion&&"Settings");
+    }
+  }
+}
