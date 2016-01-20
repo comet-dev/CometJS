08c42aa4337a034f63ff85e406ca1757396d6b24
Moses Grant <mosesgrant3345@gmail.com>
2016-01-20 15:37:20 -0500

    Update server.js

--- comet/framework/server.js
+++ comet/framework/server.js
@@ -5,8 +5,12 @@ var cometServer = new server;
 cometServer.write("1.100.COMET")&&cometServer.getServerID();
 if(cometServer === 'invalid'){
   catch(e){
-    try {
+    console.error(e +"is invalid "/n)
+  }
+   try {
       cometServer.overwrite("1.100.CAPP");
     }
+  finally {
+    cometServer.replace("1.100.CAPP");
   }
 }
