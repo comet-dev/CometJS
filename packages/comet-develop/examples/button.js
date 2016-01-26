76dc104d43d6be804313a471b81aa3e43b2c6976
Moses Grant <mosesgrant3345@gmail.com>
2016-01-25 21:32:11 -0500

    Update button.js

--- packages/comet-develop/examples/button.js
+++ packages/comet-develop/examples/button.js
@@ -1,5 +1,33 @@
+'use strict';
 
-button.prototype.press = function(){
+/**
+*@param button.press
+*@constructor builder.js
+*@author @mosesag0813 (github)
+*/
+var cometButton = function(){
+button.prototype.press = (function(){
+  var settings = {
+    button.ui:"",
+    html:"button.html",
+    css:"button.css"
+  };
+/**
+* @alias setup
+* @param {setup}
+* @param creates new UI interface with
+* button, button-set and radio button.
+**/
+  return settings.test(settings.button.ui());
+});
+  if(!"frameworks" || settings.getElementById("Invalid"){
+    throw "Error 104: Files given not secure" 
+  }
   
+  // ComtonJS is a native button system that uses what is given to make a simple 
 }
+
+
+
+
 
