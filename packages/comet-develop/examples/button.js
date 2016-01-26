b750ab6fb3b3ec521c3b2f9e286e702b5771a2c8
Moses Grant <mosesgrant3345@gmail.com>
2016-01-26 08:15:30 -0500

    Try appending child element

--- packages/comet-develop/examples/button.js
+++ packages/comet-develop/examples/button.js
@@ -4,7 +4,9 @@
 *@param button.press
 *@constructor builder.js
 *@author @mosesag0813 (github)
+*@ 
 */
+
 var cometButton = function(){
 button.prototype.press = (function(){
   var settings = {
@@ -21,10 +23,14 @@ button.prototype.press = (function(){
   return settings.test(settings.button.ui());
 });
   if(!"frameworks" || settings.getElementById("Invalid"){
-    throw "Error 104: Files given not secure" 
+    throw "Error 104: Files given not secure"
+    try {
+      settings.appendChild();
+    }
   }
+  // FrameworkJS is a native framework system that uses Comet dependencies.
   
-  // ComtonJS is a native button system that uses what is given to make a simple 
+  button.prototype.toggleON 
 }
 
 
