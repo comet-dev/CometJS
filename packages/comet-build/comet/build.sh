0de87a400c733f1505829c7c66cbfe60a9964a38
Moses Grant <mosesgrant3345@gmail.com>
2016-04-04 08:54:45 -0400

    build using bash file

---
+++ packages/comet-build/comet/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+buildPackage = "./comet-build/build"
+
+if[$buildPackage == "False"];
+  then 
+  mkdir $buildPackage 
+  find $buildPackage
+  
+addAPackage(){
+  $buildPackage == "True";
+  compress $buildPackage
+  unzip $buildPackage
+}  
+
+set $buildPackage + mkdir " .comet"
+if[mkdir "correct  " + "find new" set /bin];
+ then 
+ compress ".comet "
