8564c27cfb4de28ce5cd62614d399959e8665696
Moses Grant <mosesgrant3345@gmail.com>
2016-04-30 13:32:21 -0400

    Update error_maker.js

--- tools/error_maker.js
+++ tools/error_maker.js
@@ -1,1 +1,10 @@
-
+var error = (function{
+  var errNum = [100-299];
+  var err = require('error')
+  throw(err){
+    err.print("Error" + errNum);
+  }
+  try(err){
+    errNum.resolve(err);
+  }
+});
