612ea2932af142d406716e71725e374d4a47b47e
Moses Grant <mosesgrant3345@gmail.com>
2016-05-02 08:43:19 -0400

    Update build.js

--- packages/comet-build/build.js
+++ packages/comet-build/build.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 var repository = "";
+
 function createBuild(repo){
   var build = require("../comet-build");
   var comet = require("bin/comet")
   
-  
+  if(build(repo)&&comet.use("new build") === true){
+    
+  }
+}
