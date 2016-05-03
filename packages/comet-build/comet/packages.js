14701907d12ad63bef6de9dbe106cd170b809256
Moses Grant <mosesgrant3345@gmail.com>
2016-05-03 05:17:49 -0400

    Update packages.js

--- packages/comet-build/comet/packages.js
+++ packages/comet-build/comet/packages.js
@@ -1,11 +1,14 @@
-import CometPackage
-set "Frameworks"
+import comet from 'CometJS'
+import comet-develop from 'CometJS'
+
 var CometPackage = require("../deps.js");
 var repo = "";
+
 if(repo === CometPackage(repo)){
   repo.push(CometPackage(repo));
   repo.integrate("Comet Packages to Repo");
 }
+
 if(CometPackage(repo) === true){
   repo.alert("Package Integrated")
 }
