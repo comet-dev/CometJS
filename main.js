2d1637dda1ad2ec8ab82895b7c9f24515c55a7b5
Moses Grant <mosesgrant3345@gmail.com>
2016-05-11 19:14:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-function filerForJS(filer){
-   file = {
-      process:true
-   };
-  for(var i = 0; i<filer.length; i++){
-      filer.push(file.process);
-  }
-  if(file.process === true){
-    require("package.json");
-  }
+function organize(json, elements){
+ elements.confirm("Organize elements? (y/n)");
+ if(elements.confirm() === "yes" || "Yes" || "y" || "Y"){
+    elements.print("Organizing...");
+ }
+ json.getElementById('package.json'); 
 }
 function init(e){
-  filerForJS(e);
-  require("comet/package.js");
+  organize('package.json', '.ices');
+  e.require("comet/package.js");
   e.file("build.js");
 }
 init("comet/package.js");
-filerForJS("package.json");
+
