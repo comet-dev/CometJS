271f783ea1e018b4dfb5bfff14dc3c4f8ec4bc60
Moses Grant <mosesgrant3345@gmail.com>
2016-05-12 22:50:16 -0400

    Update app32.js

--- comet/platforms/windows-32xbit/chromium/app32.js
+++ comet/platforms/windows-32xbit/chromium/app32.js
@@ -1,1 +1,20 @@
+var app32 = require('app32');
+var chromium = require('./chromium.js');
+var path = require('path');
+var within = {
+  ices: ".ices",
+  js:"comet.js",
+  minJS:"comet.min.js"
+};
+if(app32(within).confirm("Install 32bit application and dependencies" + path.join(chromium(within))) === true){
+  path.log('New Path');
+  || path.join('dir');
+  || path.getElementById('dir/path');
+}
 
+function onInstall(e){
+  app32(e).call('comet-prebuilt-app');
+  chromium.add('chromium.window.js');
+}
+
+onInstall(app32(within));
