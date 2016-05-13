ce5f1b17b641230cc45c756e22276a6502cadf4f
Moses Grant <mosesgrant3345@gmail.com>
2016-05-12 22:55:36 -0400

    Update app64.js

--- comet/platforms/windows-64xbit/chromium/app64.js
+++ comet/platforms/windows-64xbit/chromium/app64.js
@@ -1,1 +1,20 @@
+var app64 = require('app64');
+var chromium = require('./chromium.js');
+var path = require('path');
+var within = {
+  ices: ".ices",
+  js:"comet.js",
+  minJS:"comet.min.js"
+};
+if(app64(within).confirm("Install 32bit application and dependencies" + path.join(chromium(within))) === true){
+  path.log('New Path');
+  || path.join('dir');
+  || path.getElementById('dir/path');
+}
 
+function onInstall(e){
+  app64(e).call('comet-prebuilt-app');
+  chromium.add('chromium.window.js');
+}
+
+onInstall(app64(within));
