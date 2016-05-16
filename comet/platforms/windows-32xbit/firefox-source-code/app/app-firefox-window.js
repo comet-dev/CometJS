0e280274aaf8925963c775f6004f188805898318
Moses Grant <mosesgrant3345@gmail.com>
2016-05-16 07:42:48 -0400

    Update app-firefox-window.js

--- comet/platforms/windows-32xbit/firefox-source-code/app/app-firefox-window.js
+++ comet/platforms/windows-32xbit/firefox-source-code/app/app-firefox-window.js
@@ -1,2 +1,9 @@
 var devel = require('devel-utils');
 var utils = require('utils');
+
+// Load all utilies of Firefox SC
+utils.load('firefox.js', 'browser-window-display');
+utils.display.load('prefs.js');
+utils.load('firefox-window-size', '300x500');
+utils.load('firefox-window-')
+
