95f78719a69cbd9363a5fd4dfd449273b1671f3b
Moses Grant <mosesgrant3345@gmail.com>
2016-05-25 21:17:40 -0400

    Update button.js

--- packages/comet-develop/examples/button.js
+++ packages/comet-develop/examples/button.js
@@ -1,4 +1,4 @@
-/** Copyright 2013 {name of copyright owner}
+/** Copyright 2013 CometJS. All Rights Reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
