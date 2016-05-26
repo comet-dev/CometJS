b11336b6d4cd4311603b6557a39f8daa67d0efbf
Moses Grant <mosesgrant3345@gmail.com>
2016-05-26 19:45:51 -0400

    Update and rename New Text Document.txt to button.js

---
+++ comet/framework/ui/button/button.js
@@ -0,0 +1,154 @@
+/** Copyright 2013 CometJS. All Rights Reserved.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   **/
+"use strict";
+
+/**
+*@param button.press
+*@constructor builder.js
+*@author @mosesag0813 (github)
+*@fileoverview Creates new UI interface with button, button-set and radio button.
+*/
+
+cometButton = function($){
+/** 
+ * @protected
+ * @interface Creates new UI interface using minified elements.
+**/
+
+
+button.prototype.create = function(){
+  var settings = {
+    buttonClass:"button",
+    html:"button.html",
+    css:"button.css"
+  };
+  
+  var createButton = {
+    minJs:"comet.min.js",
+    minCss:"comet.min.css",
+    buttonName:""
+  };
+/**
+* @alias setup
+* @param {setup}
+**/
+  return settings.test(settings.createButton.createAttribute("Generate a Button") && createButton.display.firstChild.innerHTML);
+ }
+ /** Buttons - Radio, Classic, Checkboxes
+ * @description CometJS organizes buttons into distinct elements. 
+ * 
+ * @description Radio - classic, primary
+ * 
+ * @description Classic - primary, secondary, danger, warning
+ * 
+ * @description Checkboxes - classic
+ * 
+ * @description The distinct elements are distributed among significance, 
+ * which is more or less the core of Desktop Frameworks. This simplifies scalabilties of UI elements within desktop apps.
+ * 
+ * @events button.prototype.create, toggle, hover, andd test.
+ **/
+ /** @protected **/
+  if("frameworks" === false || settings.getElementById("Invalid")){
+    throw "Error 104: Files given are invalid";
+    try {
+      settings.appendChild();
+    }
+    catch(err){
+      settings.status(err);
+      settings.test(err);
+    }
+  }
+  
+  // button toggle on/off
+  button.prototype.toggle = function(){
+     /** @protected **/
+    var $ui = this.$ui;
+    this.$ui = $(ui)
+    var currentVersion = "1.1.3";
+    var toggle = {
+      statusOne:"on",
+      statusTwo:"off"
+    };
+    /** @private **/
+    this.$ui.display = window.open(this.$ui.toggle.status("On"));
+    this.$ui.test("comet.min.js") && this.$ui.test("comet.min.css");
+    this.$ui.hasFeature("buttonOn") && this.$ui.getElementsByClassName("button");
+    
+   return toggle.status("on") && toggle.statusOne === true || toggle.statusTwo === false;
+  }
+  
+  /** FrameworkJS is a native framework system that uses Comet dependencies.
+   * @constructor button.createNew.ui
+   * @alias button.useNewElements
+   **/
+    var useElements = {
+      css:"button.css",
+      html:"button.html",
+      js:"button.js",
+      elements:"elements.css"
+    }
+    /** @protected **/
+    useElements.test(this.ui);
+    var buttonUI = this.ui.createElement("button");
+    this.$ui.test.StatusOne(useElements.html); 
+    button.prototype.create(useElements.getElementById("css"));
+    /** @private **/
+    button.prototype.toggle(this.$ui.test(settings));
+    this.$ui.prototype.URL.open(useElements.js, useElements.html);
+    
+    if(buttonUI === false || useElements === "Files Unknown" && useElements.getElementById("frameworks") === false){
+    try {
+      ui.test();
+      if(ui === false){
+        ui.alert("Must get new download: " + version);
+        throw "Cannot interpret files";
+      }
+     } 
+    catch(error){
+      settings.find("frameworks");
+    }
+  }
+  
+  // hover state
+  button.prototype.hover = function(){
+     /** @protected **/
+    var mouse = ui.getElementById("Mouse-Over-Button");
+    var overButton = {
+      state:"over the button"
+    };
+    mouse.backgroundColor.display = overButton.test(mouse.status("true"));
+    if(mouse === overButton.state){
+      ui.test(overButton.state.push(mouse.backgroundColor));  
+      overButton.state = true;
+      ui.innerHTML.focus(mouse);
+      console.log(mouse.status(overButton));
+      if(mouse.status(overButton) === true){
+        mouse.status(overButton.display);
+      }
+    }
+    return overButton.getAttribute(mouse);
+   } 
+   /**
+    *@module testButton
+    *@protected
+   */
+ button.prototype.testc = (function(testc){
+   var testButton = this.testButton;
+   this.testButton.parse(settings.display.status("On"));
+   this.testButton.exec(button.prototype.hover().click());
+   if(this.testButton.test(button.prototype.create() && button.prototype.toggle() === true)){
+    window.display(button.prototype.create().click());
+   }
+  })(testc);
+}
+cometButton();
