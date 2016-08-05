37e8da815e1e2c58eb1c4ee8ca497c6e9f002a8c
Moses Grant <mosesgrant3345@gmail.com>
2016-08-04 22:38:06 -0400

    Update app.js

--- comet/framework/app/app.js
+++ comet/framework/app/app.js
@@ -3,6 +3,9 @@ var app;
 app.window.URL('1.100.COMET')&&app.window.open(app);
 
 app.on("build");
-
+/**
+ * @overview Scaling apps to fit all different sizes.
+ * @todo - Constant Bugging
+ **/
 app.use(window.open(app));
 
