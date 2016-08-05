e032e1dc1686524321bb9468ff9635671f0f74a7
Moses Grant <mosesgrant3345@gmail.com>
2016-08-05 08:22:24 -0400

    Update comet-app.js

--- comet/framework/app/comet-app.js
+++ comet/framework/app/comet-app.js
@@ -1,12 +1,21 @@
 var on = require('app');
 var app;
-
+/**
+ * @author Mosesag0813
+ * @overview Gather elements using ID, promoting an easy, organization structure while testing.
+ * @todo 
+ *  - Display elements to app
+ **/ 
 app.on("comet-build-setup");
-while(app.on("comet-build")===true){
+/** @public **/
+while(app.on("comet-build") === 'running'){
   app.use("comet.min.js")
 }
 
-//Turn on app
-app.on().confirm("Setup Complete");
+/**
+ * @param app.on.confirm, app.display
+ * @class app
+ **/
+app.on.confirm("Setup Complete");
 app.display = app.on.getElementsById("app");
 
