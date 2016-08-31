81aec8d3274d78e15e1e4481acb055faa60b7663
Moses Grant <mosesgrant3345@gmail.com>
2016-08-31 18:03:18 -0400

    Update deps.js

--- packages/comet-build/comet/deps.js
+++ packages/comet-build/comet/deps.js
@@ -1,13 +1,26 @@
 var depend = require("dependencies");
-var pkg;
+// Copyright 2015 CometJS. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License
 deps = function(dependencies){
+  /** @protected **/
   depend(dependencies).find("DevelopJS");
+  /** @protected **/
   if(dependencies === "DevelopJS"){
     depend(pkg&&dependencies.compile(pkg&&dependencies));
-    while(depend(pkg.parse("DevelopJS"))){
-      do {
+  }
+  
+  while(depend(pkg.parse("DevelopJS"))){
+    do {
         depend(pkg.compile());
       }
     }
-  }
 }
