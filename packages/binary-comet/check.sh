eca6f6af41ade5d6d1dcea698a874f2b99610f17
Moses Grant <mosesgrant3345@gmail.com>
2016-09-07 07:29:23 -0400

    Update check.sh

--- packages/binary-comet/check.sh
+++ packages/binary-comet/check.sh
@@ -4,13 +4,15 @@ executable = "bin/comet"
 cd comet
 cd /bin
 if[/bin == true]
-echo "Checking for Comet executable :-) ..."
+echo "Checking for Comet executable..."
 then
   find $executable
 check_if_Executable(){
   if[$executable == true]
   then
     compress $executable
+  if[$executable -c == 'true']
+   then
     echo "Executable Compressed!"
     exit
 }
