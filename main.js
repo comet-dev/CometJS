cd3e0ba853e2287e848ff287047bbd12e1d4d65b
Moses Grant <mosesgrant3345@gmail.com>
2016-09-12 14:39:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+var usr = "usr/login";
+
  organize = function(json, elements){
- elements.confirm("Organize elements? (y/n)");
- if(elements.confirm() === "yes" || "Yes" || "y" || "Y"){
+ elements.prompt("Organize elements? (y/n)");
+ if(elements.confirm(usr.input) === "yes" || "Yes" || "y" || "Y"){
     elements.load("Organizing...");
  }
  JSON.getElementsByName('package.json'); 
 }
+
 function init(e){
   organize('package.json', '.ices');
   e.require("comet/package.js");
   e.file("build.js");
 }
-init("comet/package.js");
+init("repo/package.json");
 
