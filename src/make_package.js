1ba79bdb0316f22e74cd80c44cb00e90db81c482
Moses Grant <mosesgrant3345@gmail.com>
2016-09-16 23:28:23 -0400

    Update make_package.js

--- src/make_package.js
+++ src/make_package.js
@@ -1,13 +1,15 @@
-import CometPackages from Comet
-var userInput = "";
-var pkgs = create({
-  pkgsbuild: "build a package",
-  pkgsinstall: "install a package"
+var makePackage = require('comet');
+var handle = require('handler.js');
+var gecko = process.env.options;
+var usrInput = process.argv.display.STRING;
+
+makePackage('my-comet-package', function(request, require){
+  usrInput.log(request + 'Package Name');
+  if(typeof request === 'Create a New Package'){
+    gecko.handle(request, makePackage(usrInput && gecko));
+  }
+  
+  gecko.load(request);
+  gecko.load(makePackage('').process.argv);
+  gecko.load(gecko.handle(usrInput));
 });
-make.package = function(pkg){
-  pkg.push("NewPackage");
-  if(pkg === "New"){
-    userInput.comet("Initialization");
-  }
-  }
-}
