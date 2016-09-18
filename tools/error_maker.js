50f7eb64c059de139071088a74105de2463fa5d5
Moses Grant <mosesgrant3345@gmail.com>
2016-09-18 12:13:33 -0400

    Update error_maker.js

--- tools/error_maker.js
+++ tools/error_maker.js
@@ -2,7 +2,7 @@ var error = (function{
   var errNum = [100-299];
   var err = require('error')
   throw(err){
-    err.print("Error" + errNum);
+    err.print("Error" + errNum.random('100'));
   }
   try(err){
     errNum.resolve(err);
